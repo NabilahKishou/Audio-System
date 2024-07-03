0a8b7ab119ca8c493c850519a13bb2bacc044196
Septian Nabilah Salsabila <nabilahkishou@gmail.com>
2024-07-03 15:55:28 +0700

    [move] audio system example to samples~ folder

---
+++ Samples~/Example/Scripts/SoundSettingsModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AudioSystem.Example
+{
+    public class SoundSettingsModel
+    {
+        public VolumePreset music;
+        public VolumePreset soundEffect;
+        public VolumePreset dialog;
+    }
+}
